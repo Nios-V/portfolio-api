a29c7f4b87bc631f5b4d018a30006d3476fe4b2d
Nicolás Cáceres <niosv.1104@gmail.com>
2025-11-23 16:27:48 -0300

    feat: database/typeorm config; env config; global config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
@@ -8,8 +10,25 @@ import { ExperiencesModule } from './experiences/experiences.module';
 import { GoalsModule } from './goals/goals.module';
 
 @Module({
-  imports: [ProfilesModule, SkillsModule, ProjectsModule, ExperiencesModule, GoalsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    ProfilesModule,
+    SkillsModule,
+    ProjectsModule,
+    ExperiencesModule,
+    GoalsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
