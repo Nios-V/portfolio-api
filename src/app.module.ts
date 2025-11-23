030f69e67c61ca88bcd8ef2b87a1149cefbed12f
Nicolás Cáceres <niosv.1104@gmail.com>
2025-11-23 16:11:27 -0300

    feat: add experiences and goals modules with CRUD functionality; include skills, projects and profile modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilesModule } from './profiles/profiles.module';
+import { SkillsModule } from './skills/skills.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ExperiencesModule } from './experiences/experiences.module';
+import { GoalsModule } from './goals/goals.module';
 
 @Module({
-  imports: [],
+  imports: [ProfilesModule, SkillsModule, ProjectsModule, ExperiencesModule, GoalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
